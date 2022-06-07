2b9381a3cfb2ce23dcc5eb224a3ea8634f1313d1
divakotova <divakova.dasha@yandex.by>
2022-06-07 21:47:36 +0300

    fix: fix mentors remarks

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -6,6 +6,12 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,6 +23,12 @@ import { ReactiveFormsModule } from '@angular/forms';
     CoreModule,
     ReactiveFormsModule,
     HttpClientModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    }),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    StoreRouterConnectingModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
